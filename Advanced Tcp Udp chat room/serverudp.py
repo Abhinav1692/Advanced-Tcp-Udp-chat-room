6d2890207910112e91bd4263033315f790d2d437
Abhinav1692 <89593847+Abhinav1692@users.noreply.github.com>
2021-12-24 15:21:36 +0530

    first commit

---
+++ Advanced Tcp Udp chat room/serverudp.py
@@ -0,0 +1,23 @@
+import socket
+
+if __name__ == "__main__":
+    host = "127.0.0.1"
+    port = 4455
+
+    """ Creating the UDP socket """
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    """ Bind the host address with the port """
+    server.bind((host, port))
+
+    while True:
+        data, addr = server.recvfrom(1024)
+        data = data.decode("utf-8")
+        print(data)
+
+        print(f"Client: {data}")
+
+        # data = data.upper()
+        data = input("Enter a word: ")
+        data = data.encode("utf-8")
+        server.sendto(data, addr)
